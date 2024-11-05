4c49904bba27c9669917c175688b83aceb7f6736
Sean O'Leary <sean@o-leary.co.nz>
2024-11-05 18:10:23 +1300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
+if [ ! -f /etc/wgdashboard/src/wgd.sh ]; then
+        git clone -q https://github.com/donaldzou/WGDashboard.git /etc/wgdashboardtmp
+        cp -r /etc/wgdashboardtmp/* /etc/wgdashboard/
+        rm /etc/wgdashboardtmp -R
+        cd /etc/wgdashboard/src
+        ./wgd.sh install
+fi
+
 cd /etc/wgdashboard/src
 ./wgd.sh start
 sleep infinity
